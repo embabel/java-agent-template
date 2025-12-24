9400aeb50a9edd2a4fa87b8bc7fbf8ecd5d2d986
Rod Johnson <johnsonroda@gmail.com>
2025-12-24 16:16:10 +1000

    Remove EnableAgents

--- src/main/java/com/embabel/template/ProjectNameApplication.java
+++ src/main/java/com/embabel/template/ProjectNameApplication.java
@@ -15,14 +15,10 @@
  */
 package com.embabel.template;
 
-import com.embabel.agent.config.annotation.EnableAgents;
-import com.embabel.agent.config.annotation.LoggingThemes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
-@EnableAgents(loggingTheme = LoggingThemes.STAR_WARS)
 class ProjectNameApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProjectNameApplication.class, args);
